6b5df5aaea45083fb5c712b96562b4522679f2c1
AnicetFantomas <94958024+AnicetFantomas@users.noreply.github.com>
2022-05-14 08:02:59 +0200

    Update files with api

--- src/Redux/Books/Books.js
+++ src/Redux/Books/Books.js
@@ -1,39 +1,61 @@
 /* eslint-disable no-unused-vars */
 const ADD = 'ADD_BOOK';
 const REMOVE = 'REMOVE_BOOK';
-const booksInitial = [];
+const FETCHBOOK = 'FETCHBOOK';
+const FETCHERROR = 'FETCHERROR';
 
-export const addNewBook = (title, author, id) => {
-  booksInitial.push({
-    id,
-    author,
-    title,
-    // completed,
-  });
+const booksInitial = {
+  bookStore: [],
+  loadingAnimation: true,
+  error: false,
 };
 
-let ID = 0;
-export const remove = (id) => {
-  ID = id;
-};
+export const addNewBook = (title, author, id) => ({
+  id,
+  category: 'action',
+  author,
+  title,
+});
 
 const bookReducer = (state = booksInitial, action) => {
   switch (action.type) {
+    case FETCHBOOK:
+      return [...state];
     case ADD:
-      return [...booksInitial];
+      return {
+        bookStore: action.payloads,
+        loadingAnimation: false,
+      };
     case REMOVE:
-      return [...state.filter((book) => book.id !== ID)];
+      return [...state.filter((book) => book.item_id !== action.payloads)];
+
+    case FETCHERROR:
+      return {
+        error: true,
+      };
 
     default: return state;
   }
 };
 
-export const addBook = () => ({
+export const addBook = (bookItems) => ({
   type: ADD,
+  payloads: bookItems,
 });
 
-export const removeBook = () => ({
+export const removeBook = (bookItems) => ({
   type: REMOVE,
+  payloads: bookItems,
+});
+
+export const fetchBooks = () => (
+  {
+    type: FETCHBOOK,
+  }
+);
+
+export const fetchBookErr = () => ({
+  type: FETCHERROR,
 });
 
 export default bookReducer;
--- src/components/BooksList/BooksList.js
+++ src/components/BooksList/BooksList.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { generate } from 'randomized-string';
 import { alphanumeric } from 'randomized-string/lib/types';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import BookItem from '../BookItem/BookItem';
 import BookForm from '../BookForm/BookForm';
+import { getBookApi } from '../../Redux/Books/BooksApi';
 
 const BooksList = () => {
-  const bookSelct = useSelector((state) => state.bookReducer);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBookApi());
+  }, [dispatch]);
+
+  const { bookStore, loadingAnimation, error } = useSelector((state) => state.bookReducer);
+
+  if (loadingAnimation === true) {
+    return (<div><img src="https://i.pinimg.com/originals/f6/06/cb/f606cbf26c0a18898b96ef6857953a75.gif" className="loading" alt="loading" /></div>);
+  }
+
+  if (error === true) {
+    return (<div className="error">Error While trying to fetch, Try refreshing the page</div>);
+  }
+
   return (
     <div>
       <ul>
-        {bookSelct.map((book) => (
-          <BookItem book={book} key={generate({ charset: alphanumeric, length: 32 })} />
+        {Object.keys(bookStore).map((book) => (
+          <BookItem
+            ID={book}
+            book={bookStore[book][0]}
+            key={generate({ charset: alphanumeric, length: 32 })}
+          />
         ))}
       </ul>
       <div>
